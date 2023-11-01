7fcff75940fc883dee360a5d1da00c55a5b11db7
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-01 06:00:31 +0000

    fix inser left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,24 +1,49 @@
 #include "binary_trees.h"
 /**
- * binary_tree_insert_left - inserts a new node at the left child of parent
- * @parent: the parent node to insert the new left child node
- * @value: value of the new node
- * Return: pointer to new node or null
+ * binary_tree_node - creates a binary tree node
+ * @parent: parent node of the created node
+ * @value: value to go inside the created noe
+ * Return: pointer to created node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
-	if (newNode == NULL || parent == NULL)
+	if (newNode == NULL)
 		return (NULL);
 
-	newNode->n = value;
 	newNode->parent = parent;
-	newNode->left = parent->left;
+	newNode->n = value;
+
+	newNode->left = NULL;
 	newNode->right = NULL;
 
-	if (newNode->left != NULL)
-		newNode->left->parent = newNode;
+	return (newNode);
+}
+
+
+/**
+ * binary_tree_insert_left - inserts a node as the left-child of another node
+ * @parent: pointer to the parent node to insert the left-child in
+ * @value: value to store in the new node
+ * Return: pointer to the created node, or NULL on failure or if parent is NULL
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode;
+
+	if (parent == NULL)
+		return (NULL);
+
+	newNode = binary_tree_node(parent, value);
+	if (newNode == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		newNode->left = parent->left;
+		parent->left->parent = newNode;
+	}
 
 	parent->left = newNode;
 
