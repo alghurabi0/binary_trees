95cddaa5c4849eb43bc3a762b685be93c3127b34
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-01 06:20:16 +0000

    fix balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,11 +26,13 @@ int binary_tree_balance(const binary_tree_t *tree)
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t left_height, right_height;
 	if (tree == NULL)
 		return (0);
-
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return ((left_height > right_height ? left_height : right_height) + 1);
 }
