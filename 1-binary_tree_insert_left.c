7ba06aabd434a03f8302b0b2cb5d2b9a7b64a401
Ubuntu <ubuntu@ip-172-31-84-171.ec2.internal>
2023-11-01 06:02:05 +0000

    another fix

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,28 +1,5 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - creates a binary tree node
- * @parent: parent node of the created node
- * @value: value to go inside the created noe
- * Return: pointer to created node
- */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
-{
-	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
-
-	if (newNode == NULL)
-		return (NULL);
-
-	newNode->parent = parent;
-	newNode->n = value;
-
-	newNode->left = NULL;
-	newNode->right = NULL;
-
-	return (newNode);
-}
-
-
-/**
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: pointer to the parent node to insert the left-child in
  * @value: value to store in the new node
